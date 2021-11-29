1de94ca339bc78cc3e94205ade14e50c2ed309b1
Ross <komarostyslav@gmail.com>
2021-11-29 15:31:19 +0200

    Rename/Delete of the models && adding to admin

--- reader/registration/models.py
+++ reader/registration/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 
-class RegistrationData(models.Model):
+class UsersData(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
-    email = models.EmailField(max_length=250)
+    email = models.EmailField(max_length=250, default=None)
     username = models.CharField(max_length=250)
     password = models.CharField(max_length=100)
-    registration_date = models.DateField(auto_now=True)
--- reader/registration/views.py
+++ reader/registration/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-def login(request):
+
+def login_user(request):
+    # username = request.POST['user']
+    # password = request.POST['password']
+    # user = authenticate(request, username=username, password=password)
     return render(request, 'registration/login_page.html')
